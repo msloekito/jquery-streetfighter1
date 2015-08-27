b0d18aeedcbc60bfc75900f103517cef17a15b92
msloekito <msloekito@gmail.com>
2015-08-26 18:00:10 -0700

    added sound

--- js/app.js
+++ js/app.js
@@ -8,16 +8,28 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left':'1020px'}, 500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px')
+			}
+			);
 		//play hadouken sound
 		//show hadouken and animate it to the right of the screen
 	})
 	.mouseup(function() {
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
-		$('.hadouken').hide()
+		$('.hadouken').hide	()
 		//ryu goes back to ready position
 	});
 });
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
