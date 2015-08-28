927bdb696cc719cf075dd3e4b2cde021ad38d2e7
msloekito <msloekito@gmail.com>
2015-08-27 21:34:16 -0700

    complete assignment

--- js/app.js
+++ js/app.js
@@ -2,34 +2,70 @@ $(document).ready(function(){
 	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
+	});
+	$('.ryu').mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	})
-	.mousedown(function() {
+	});
+	$('.ryu').mousedown(function(){
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
-			{'left':'1020px'}, 500,
+			{'left':'1020px', 'top':'500px'}, 500,
 			function() {
-				$(this).hide();
-				$(this).css('left', '520px')
-			}
-			);
+				$('.hadouken').hide();
+				$('.hadouken').css('left', '520px');
+				$('.hadouken').css('top', '170px');
+	})
+	});
 		//play hadouken sound
 		//show hadouken and animate it to the right of the screen
-	})
-	.mouseup(function() {
+	$('.ryu').mouseup(function(){
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
-		$('.hadouken').hide	()
+		$('.hadouken').hide();
 		//ryu goes back to ready position
 	});
-});
+
+	$(document).keydown(function(e)
+	{
+		if(e.keyCode == 88)
+		{
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+
+			console.log("cool pose");
+		};
+
+	});
+
+	/*$('.ryu').mouseenter().keyup(function()
+	{
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+			$('.ryu-cool').hide();
+			console.log("mouse over ryu");
+		
+	});	*/
+
+	$(document).keyup(function()
+	{
+			$('.ryu-still').show();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').hide();
+			console.log("keyup registered");
+			$('.ryu').mouseenter(function(){
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+			console.log("over ryu");	
+	});
+		
+	});	
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
+})
